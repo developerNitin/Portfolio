c6d2f87a210c01252e052eddf3814a0b1f1ca76f
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-20 19:45:21 +0530

    custom-cursor-added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/app"
 
+
+
 ReactDOM.render(
-    <App />
-    ,
-    document.getElementById("root")
+  <App />
+  ,
+  document.getElementById("root")
 )
