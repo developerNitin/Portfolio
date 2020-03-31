21dc0aa7fde1d1e4caa59fd8cbca3a5e002d004f
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-31 19:52:12 +0530

    commit

--- src/components/line.jsx
+++ src/components/line.jsx
@@ -1,28 +1,9 @@
 import React from "react";
+import "./css/line.css"
 
 function Line() {
-  const line = {
-    position: "absolute",
-    width: "1px",
-    height: "250%",
-    backgroundColor: "rgb(155, 120, 222)",
-    left: "19.5%",
-    top: "36.4%",
-  };
-
-  const span = {
-    border: "2px solid rgb(155, 120, 222)",
-    width: "5px",
-    height: "5px",
-    position: "absolute",
-    borderRadius: "50%",
-    display: "block",
-    top: "-7px",
-    left: "-4px"
-  }
-  
-  return <div style={line}>
-    <span style={span}></span>
+  return <div className="line">
+    <span className="span"></span>
   </div>;
 }
 
