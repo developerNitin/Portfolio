dd83e9bf959a291fd3517fde75ceed9c14d0927c
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-07 18:16:21 +0530

    commit

--- src/components/touch.jsx
+++ src/components/touch.jsx
@@ -4,47 +4,51 @@ import "./css/touch.css";
 function Touch() {
   return (
     <div id="touch">
-      <h1 className="touch-heading">
+      {/* <h1 className="touch-heading">
         feel free to{" "}
         <span style={{ color: "rgb(155, 120, 222)" }}>Disturb me</span>
-      </h1>
-      <div className="contact-heading">
-        <div>
-          <h1>Let's talk</h1>
-          <p>Ask us anything or just say hi...</p>
-          <span>
-            <h3>+91-8847379603</h3>
-            <h4>nitin.birdi@icloud.com</h4>
-          </span>
+      </h1> */}
+      <div id="sub-touch">
+        <div className="contact-heading">
+          <div>
+            <h1>Let's talk</h1>
+            <p>Ask us anything or just say hi</p>
+            <span>
+              <h3>+91-8847379603</h3>
+              <h4>nitin.birdi@icloud.com</h4>
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="contact-form">
-        <div>
-          <form>
-            <label>NAME</label> <label>EMAIL</label> <br />
-            <input
-              className="top-two input1"
-              type="text"
-              placeholder="john doe"
-              required="required"
-            ></input>
-            <input
-              className="top-two input3"
-              type="email"
-              placeholder="example@yourdomain.com"
-              required
-            ></input>{" "}
-            <br />
-            <label className="message-label">MESSAGE</label> <br />
-            <input
-              className="message input3"
-              type="text"
-              placeholder="Hi there"
-              required
-            ></input>{" "}
-            <br />
-            <button type="submit">SEND</button>
-          </form>
+        <div className="contact-form">
+          <div>
+            <form>
+              <div>
+                <label>NAME</label> <label className="email-label">EMAIL</label> <br />
+                <input
+                  className="top-two input1"
+                  type="text"
+                  placeholder="john doe"
+                  required="required"
+                ></input>
+                <input
+                  className="top-two input2"
+                  type="email"
+                  placeholder="example@yourdomain.com"
+                  required
+                ></input>{" "}
+                <br />
+                <label className="message-label">MESSAGE</label> <br />
+                <input
+                  className="message input3"
+                  type="text"
+                  placeholder="Hi there"
+                  required
+                ></input>{" "}
+                <br />
+                <button type="submit">SEND</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
