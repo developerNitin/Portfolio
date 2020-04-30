2f5512e29068e9ae95582f000d5f8c29034d8169
Nitin Birdi <nitin.birdi@icloud.com>
2020-04-30 18:27:09 +0530

    Code

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,28 +1,89 @@
 import React from "react";
-import Img from "./img/coding.svg"
+import Img from "./img/coding.svg";
 import "./css/home.css";
 
 function Home() {
-  function visi() {
-    document.getElementById("scroll-mention").style.display = "block";
-  }
+  let darkGray = "rgb(46, 42, 47)";
+  let gray = "rgb(66, 64, 67)";
+  let purple = "rgb(155, 120, 222)";
+
+  let home = {
+    height: "calc(100vh)",
+    width: "100%",
+    backgroundColor: darkGray,
+    margin: "0",
+    padding: "0",
+    scrollSnapAlign: "start",
+  };
+
+  let intro_div = {
+    position: "absolute",
+    textAlign: "left",
+    marginLeft: "10%",
+    marginTop: "30vh",
+    color: "rgb(226, 226, 226)",
+  };
+
+  let h1 = {
+    fontSize: "60px",
+    margin: "0",
+    transition: "all .4s ease",
+    fontFamily: '"mon500"',
+  };
+
+  let p = {
+    fontSize: "19px",
+    marginTop: "10px",
+    transition: "all .4s ease",
+    fontFamily: '"mon400"',
+  };
+
+  let scroll_mention = {
+    position: "absolute",
+    width: "100%",
+    bottom: "0",
+    fontSize: "14px",
+    textAlign: "center",
+  };
+
+  let h3 = {
+    margin: "0",
+    color: "white",
+    opacity: "0.8",
+    fontFamily: '"mon200"',
+    height: "20px",
+  };
+
+  let coding_img = {
+    width: "400px",
+    position: "absolute",
+    top: "22vh",
+    right: "10vw",
+  };
+
+  let svg = {
+    margin: "5px 0 -3px 0",
+    fill: gray,
+    opacity: "0.7",
+    marginBottom: "10px",
+  };
 
   return (
-    <div id="home" onMouseEnter={visi}>
-      <div className="intro-div">
-        <h1 id="h1">
+    <div id="home" style={home}>
+      <div className="intro-div" style={intro_div}>
+        <h1 id="h1" style={h1}>
           Hi,
           <br /> it's<span> Nitin </span>
         </h1>
-        <p>
+        <p style={p}>
           I'm a Web, Ios and Android Engineer who helps <br />
           to turn ideas into accessible experiences.
         </p>
       </div>
-      <img className="coding-img" src={Img}></img>
-      <footer id="scroll-mention">
-        <h3>start scrolling</h3>
-        <svg width="150" height="3">
+      <img className="coding-img" style={coding_img} src={Img}></img>
+      <footer id="scroll-mention" style={scroll_mention}>
+        <h3 style={h3}>start scrolling</h3>
+        <svg width="150" height="3" style={svg}>
           <rect width="150" height="3"></rect>
         </svg>
       </footer>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./css/navbar.css";
 
 function Navbar() {
   const [link, setLink] = useState(true);
@@ -10,21 +9,25 @@ function Navbar() {
     y1: "13",
     x2: "11",
     y2: "27",
-    w: "20"
+    w: "20",
   });
 
   function visible() {
     setLink(true);
     document.querySelector("#links").style.visibility = "hidden";
     document.querySelector(".btn-rect5").style.visibility = "visible";
+    let bun_rect_1 = document.querySelector(".bun-rect-1")
+    let bun_rect_2 = document.querySelector(".bun-rect-2")
+    bun_rect_1.style.transform = "rotate(0)"
+    bun_rect_1.style.WebkitTransform = "rotate(0)"
+    bun_rect_2.style.transform = "rotate(0)"
+    bun_rect_2.style.WebkitTransform = "rotate(0)"
     setRect({
-      Class1: "bun-rect1",
-      Class2: "bun-rect2",
       x1: "11",
       y1: "13",
       x2: "11",
       y2: "27",
-      w: "20"
+      w: "20",
     });
   }
 
@@ -32,22 +35,43 @@ function Navbar() {
     setLink(false);
     document.querySelector("#links").style.visibility = "visible";
     document.querySelector(".btn-rect5").style.visibility = "hidden";
+    let bun_rect_1 = document.querySelector(".bun-rect-1")
+    let bun_rect_2 = document.querySelector(".bun-rect-2")
+    bun_rect_1.style.transform = "rotate(45deg)"
+    bun_rect_1.style.WebkitTransform = "rotate(45deg)"
+    bun_rect_2.style.transform = "rotate(-45deg)"
+    bun_rect_2.style.WebkitTransform = "rotate(-45deg)"
     setRect({
-      Class1: "bun-rect3",
-      Class2: "bun-rect4",
       x1: "19",
       y1: "-1",
       x2: "-12",
       y2: "29",
-      w: "23"
+      w: "23",
     });
   }
+  let bun_rect1 = { WebkitTransform: "rotate(0)", transform: "rotate(0)" };
+  let bun_rect2 = { WebkitTransform: "rotate(0)", transform: "rotate(0)" };
+  let bun_rect3 = {
+    WebkitTransform: "rotate(45deg)",
+    transform: "rotate(45deg)",
+  };
+  let bun_rect4 = {
+    WebkitTransform: "rotate(-45deg)",
+    transform: "rotate(-45deg)",
+  };
 
   function rect(Class1, Class2, x1, y1, x2, y2, w) {
     return (
-      <svg height="40" width="40">
+      <svg
+        height="40"
+        width="40"
+        style={bunBurger_svg}
+        onMouseEnter={bunBurger_hover_svg}
+        onMouseLeave={bunBurger_hoverout_svg}
+        className="bunburger_svg"
+      >
         <rect
-          className={Class1}
+          className="bun-rect-1"
           height="2"
           width={w}
           rx="1"
@@ -64,7 +88,7 @@ function Navbar() {
           y="20"
         ></rect>
         <rect
-          className={Class2}
+          className="bun-rect-2"
           height="2"
           width={w}
           rx="1"
@@ -75,21 +99,148 @@ function Navbar() {
     );
   }
 
-  var btns = document.querySelectorAll(".link-btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
+  function li(href, name) {
+    return (
+      <li className="link-btn" style={links_ul_li}>
+        <a
+          href={href}
+          style={links_ul_li_a}
+          className="links_ul_li_a_hover"
+          onMouseEnter={Links_ul_li_a_hover}
+          onMouseLeave={Links_ul_li_a_hoverout}
+        >
+          {name}
+          <svg
+            style={links_ul_li_svg}
+            height="2"
+            width="20"
+            className="links_ul_li_a_svg_hover"
+          >
+            <rect rx="1" height="2" width="20"></rect>
+          </svg>
+        </a>
+      </li>
+    );
+  }
+
+  // var btns = document.querySelectorAll(".link-btn");
+  // for (var i = 0; i < btns.length; i++) {
+  //   btns[i].addEventListener("click", function () {
+  //     var current = document.getElementsByClassName("active");
+  //     current[0].className = current[0].className.replace(" active", "");
+  //     this.className += " active";
+  //   });
+  // }
+
+  let navbar = { backgroundColor: "inherit" };
+  let links = {
+    height: "380px",
+    width: "217px",
+    right: "0",
+    position: "fixed",
+    visibility: "hidden",
+    zIndex: "100",
+    backgroundColor: "#363138",
+  };
+  let links_ul = {
+    height: "255px",
+    width: "173.16px",
+    fontSize: "17px",
+    listStyle: "none",
+    fontFamily: '"mon300"',
+    padding: "0",
+    textAlign: "right",
+    position: "absolute",
+    top: "70px",
+    right: "15px",
+  };
+  let links_ul_li = { height: "51px" };
+  let links_ul_li_a = {
+    letterSpacing: "1px",
+    padding: "12px 10px",
+    textDecoration: "none",
+    color: "white",
+  };
+  let links_ul_li_svg = {
+    position: "absolute",
+    fill: "white",
+    right: "-23px",
+    marginTop: "10px",
+  };
+
+  function Links_ul_li_a_hover() {
+    document.querySelector(".links_ul_li_a_hover").style.color = "white";
+    document.querySelector(".links_ul_li_a_svg_hover").style.fill = "white";
+  }
+
+  function Links_ul_li_a_hoverout() {
+    document.querySelector(".links_ul_li_a_hover").style.color = "white";
+    document.querySelector(".links_ul_li_a_svg_hover").style.fill = "white";
+  }
+
+  let bun_sun = {
+    width: "96px",
+    height: "44px",
+    top: "0",
+    right: "0",
+    position: "fixed",
+    padding: "15px 15px 0px 0",
+    zIndex: "200",
+    textAlign: "right",
+  };
+  let sun = {
+    opacity: "0.7",
+    height: "40px",
+    width: "40px",
+    borderStyle: "none",
+    backgroundColor: "inherit",
+    padding: "0",
+    top: "16px",
+    position: "absolute",
+    display: "block",
+    right: "60px",
+  };
+  let sun_svg = { backgroundColor: "inherit", fill: "white" };
+  function sun_hover_svg() {
+    document.querySelector(".sun_svg").style.fill = "#9b78de";
+  }
+  function sun_hoverout_svg() {
+    document.querySelector(".sun_svg").style.fill = "white";
+  }
+  let bunBurger = {
+    opacity: "0.9",
+    height: "40px",
+    width: "40px",
+    padding: "0",
+    borderStyle: "none",
+    backgroundColor: "inherit",
+  };
+  let bunBurger_svg = {
+    backgroundColor: "inherit",
+    fill: "white",
+    WebkitTransform: "all 0.3s ease",
+    transform: "all 0.3s ease",
+  };
+  function bunBurger_hover_svg() {
+    document.querySelector(".bunburger_svg").style.fill = "#9b78de";
+  }
+  function bunBurger_hoverout_svg() {
+    document.querySelector(".bunburger_svg").style.fill = "white";
   }
 
 
   return (
-    <nav id="navbar">
-      <div id="bun-sun">
-        <button id="sun">
-          <svg height="27" width="25">
+    <nav style={navbar}>
+      <div style={bun_sun}>
+        <button style={sun}>
+          <svg
+            height="27"
+            width="25"
+            className="sun_svg"
+            style={sun_svg}
+            onMouseEnter={sun_hover_svg}
+            onMouseLeave={sun_hoverout_svg}
+          >
             <circle cx="12.8" cy="14" r="4"></circle>
             <rect
               className="svg-sun-ray"
@@ -164,7 +315,7 @@ function Navbar() {
             ></rect>
           </svg>
         </button>
-        <button id="bunBurger" onClick={link ? hidden : visible}>
+        <button style={bunBurger} onClick={link ? hidden : visible}>
           {rect(
             Rect.Class1,
             Rect.Class2,
@@ -176,53 +327,13 @@ function Navbar() {
           )}
         </button>
       </div>
-      <div id="links">
-        <ul id="myLinks">
-          <li className="link-btn active">
-            <a href="#home">
-              intro
-              <svg
-                style={{ color: "rgb(155, 120, 222)" }}
-                height="2"
-                width="20"
-              >
-                <rect rx="1" height="2" width="20"></rect>
-              </svg>
-            </a>
-          </li>
-          <li className="link-btn ">
-            <a href="#work">
-              work
-              <svg height="2" width="20">
-                <rect rx="1" height="2" width="20"></rect>
-              </svg>
-            </a>
-          </li>
-          <li className="link-btn ">
-            <a href="#about">
-              about
-              <svg height="2" width="20">
-                <rect rx="1" height="2" width="20"></rect>
-              </svg>
-            </a>
-          </li>
-          <li className="link-btn ">
-            <a href="#skills">
-              skills
-              <svg height="2" width="20">
-                <rect rx="1" height="2" width="20"></rect>
-              </svg>
-            </a>
-          </li>
-          <li className="link-btn ">
-            <a href="#touch">
-              contact
-              <svg height="2" width="20">
-                <rect rx="1" height="2" width="20"></rect>
-              </svg>
-            </a>
-          </li>
-          {/* <div className="nav-line"></div> */}
+      <div id="links" style={links}>
+        <ul id="myLinks" style={links_ul}>
+          {li("#home", "intro")}
+          {li("#work", "work")}
+          {li("#about", "about")}
+          {li("#skills", "skills")}
+          {li("#touch", "contact")}
         </ul>
       </div>
     </nav>
